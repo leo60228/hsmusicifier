9b0f3b6ade4d8aa7dc5d0c480a94d465047d48d0
leo60228 <leo@60228.dev>
2021-01-24 16:38:03 -0500

    add gmp6 to appimage-run

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs; mkShell {
-  buildInputs = [ pkgconfig gtk3 gsettings-desktop-schemas ffmpeg appimage-run squashfsTools zip wrapGAppsHook ] ++ ffmpeg.buildInputs;
+  buildInputs = [ pkgconfig gtk3 gsettings-desktop-schemas ffmpeg squashfsTools zip wrapGAppsHook (appimage-run.override {
+    extraPkgs = pkgs: with pkgs; [ gmp6 ];
+  }) ] ++ ffmpeg.buildInputs;
 
   GSETTINGS_DESKTOP_SCHEMAS = "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}";
 
