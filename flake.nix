049220c3bfd52df9b0e79186af4a47b653b81d22
leo60228 <leo@60228.dev>
2021-02-01 11:06:57 -0500

    use flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs.naersk.url = "github:leo60228/naersk/fetchgit-submodules";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    flake = false;
+  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, rust-overlay, naersk, gitignore, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlay ];
+      };
+      gitignore-lib = import gitignore { inherit (pkgs) lib; };
+      inherit (gitignore-lib) gitignoreSource;
+      rust = pkgs.rust-bin.nightly.latest.rust;
+      naersk-lib = naersk.lib.x86_64-linux.override {
+        cargo = rust;
+        rustc = rust;
+      };
+    in rec {
+      packages = rec {
+        hsmusicifier = naersk-lib.buildPackage {
+          root = gitignoreSource ./.;
+          nativeBuildInputs = with pkgs; [ pkgconfig wrapGAppsHook git llvmPackages.llvm ];
+          buildInputs = with pkgs; [ gtk3 gsettings-desktop-schemas ffmpeg zip openssl stdenv.cc.libc ];
+          override = x: (x // {
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+            preConfigure = ''
+            export BINDGEN_EXTRA_CLANG_ARGS="-isystem ${pkgs.clang}/resource-root/include $NIX_CFLAGS_COMPILE"
+            '';
+          });
+        };
+      };
+
+      defaultPackage = packages.hsmusicifier;
+
+      devShell = with pkgs; mkShell {
+        inputsFrom = packages.hsmusicifier.builtDependencies ++ [ ffmpeg ];
+        buildInputs = [ squashfsTools (appimage-run.override {
+          extraPkgs = pkgs: with pkgs; [ gmp6 ];
+        }) ];
+
+        GSETTINGS_DESKTOP_SCHEMAS = "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}";
+        APPIMAGE_RUN = "${appimage-run}/bin/appimage-run";
+      };
+    }
+  );
+}
