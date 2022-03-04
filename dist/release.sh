6d1ade8da9a3c34f9b83357c3073ac90da087844
leo60228 <leo@60228.dev>
2022-03-03 21:26:53 -0500

    upd8 release scripts

--- dist/release.sh
+++ dist/release.sh
@@ -3,11 +3,11 @@ set -ex
 
 appimage_add="$(readlink -f "$(dirname "$0")/appimage-add.sh")"
 windows_zip="$(readlink -f "$1")"
-ffmpeg="$(readlink -f "$2")"
-linux_zip="$(readlink -f "$3")"
-appimage_rt="$(readlink -f "$4")"
-bandcamp_json="$(readlink -f "$5")"
-hsmusic="$(readlink -f "$6")"
+linux_zip="$(readlink -f "$2")"
+appimage_rt="$(readlink -f "$3")"
+bandcamp_json="$(readlink -f "$4")"
+hsmusic_data="$(readlink -f "$5")"
+hsmusic_media="$(readlink -f "$6")"
 
 cd "$(dirname "$0")/../target"
 rm -rf dist
@@ -24,13 +24,11 @@ mkdir res
 pushd res
 cp "$bandcamp_json" .
 
-mkdir hsmusic
-pushd hsmusic
-mkdir media
-cp -r "$hsmusic/media/album-art" media/
-mkdir data
-cp -r "$hsmusic/data/album" data/
-popd
+mkdir hsmusic-data
+cp -r "$hsmusic_data/album" hsmusic-data/
+
+mkdir hsmusic-media
+cp -r "$hsmusic_media/album-art" hsmusic-media/
 
 zip -r ../hsmusicifier-data.zip *
 popd
@@ -39,7 +37,6 @@ mkdir hsmusicifier-win
 pushd hsmusicifier-win
 cp ../bin/hsmusicifier.exe .
 cp -r ../res/* .
-cp "$ffmpeg"/bin/*.dll .
 popd
 
 zip -r hsmusicifier-win.zip hsmusicifier-win/
